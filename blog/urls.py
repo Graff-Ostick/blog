fb68d76db5e28fa517fb71c7bf44b4a845803932
Shebak Ruslan <rusya1998shebak@gmail.com>
2020-08-04 22:40:25 +0300

    add page nelka

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('nelka', views.show_nelka, name='nelka'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_html.html', {})
+
+def show_nelka(request):
+    return render(request, 'blog/nelka.html', {})
