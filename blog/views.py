59ca44934df1616552a926e6f55361d1226d64d9
Shebak Ruslan <rusya1998shebak@gmail.com>
2020-08-06 23:12:58 +0300

    add posts from commandline add pattern

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_html.html', {})
+    posts = Post.objects.filter(published__date_lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_html.html', {'posts':posts})
 
 def show_nelka(request):
     return render(request, 'blog/nelka.html', {})
