15fd6acdff030ab00604676c2a7ad2fe766003ab
Shebak Ruslan <rusya1998shebak@gmail.com>
2020-08-11 23:25:54 +0300

    add functionality to show posts

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Post
 from django.utils import timezone
 
 def post_list(request):
-    posts = Post.objects.filter(published__date_lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_html.html', {'posts':posts})
 
 def show_nelka(request):
